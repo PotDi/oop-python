50b73dd3fa95714b5ca44c87460e44dc5bd70d99
Dmitrii <dimasik007@rambler.ru>
2023-06-11 15:06:02 +0300

    Добавил класс CopyFile и метода класса copyfile

--- lab01/copyfile/copyfile.py
+++ lab01/copyfile/copyfile.py
@@ -1,10 +1,18 @@
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
 
-with open(input_file, encoding="UTF-8") as first_file:
-    data = first_file.read()
+class CopyFile:
+    def __init__(self):
+        self.input_file = sys.argv[1]
+        self.output_file = sys.argv[2]
 
-with open(output_file, mode="w", encoding="UTF-8") as second_file:
-        second_file.write(data)
+    def copyfile(self):
+        with open(self.input_file, encoding="UTF-8") as first_file:
+            data = first_file.read()
+        with open(self.output_file, mode="w",
+                  encoding="UTF-8") as second_file:
+            second_file.write(data)
+
+
+copy = CopyFile()
+copy.copyfile()

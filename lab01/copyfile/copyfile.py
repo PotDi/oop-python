45e961d2bb6011d346aed1de4b2e525d641c1726
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-06-14 09:23:53 +0300

    Доработал код в copyfile.py

--- lab01/copyfile/copyfile.py
+++ lab01/copyfile/copyfile.py
@@ -2,17 +2,18 @@ import sys
 
 
 class CopyFile:
-    def __init__(self):
-        self.input_file = sys.argv[1]
-        self.output_file = sys.argv[2]
+    def __init__(self, input_file: str, output_file: str):
+        self.input_file = input_file
+        self.output_file = output_file
 
     def copyfile(self):
-        with open(self.input_file, encoding="UTF-8") as first_file:
+        with open(self.input_file, mode="rb") as first_file: #читаем по
+            # байтам
             data = first_file.read()
-        with open(self.output_file, mode="w",
-                  encoding="UTF-8") as second_file:
+        with open(self.output_file, mode="wb") as second_file: #записываем
+            # по байтам
             second_file.write(data)
 
 
-copy = CopyFile()
+copy = CopyFile(input_file=sys.argv[1], output_file=sys.argv[2])
 copy.copyfile()

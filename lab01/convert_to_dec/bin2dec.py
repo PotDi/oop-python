6e5633532f1a47c7dd991bc63328833a2b57c657
Dmitrii <dimasik007@rambler.ru>
2023-06-15 22:45:14 +0300

    Небольшой рефакторинг. Переименовал файл converter_bin_to_dec.py

--- lab01/convert_to_dec/bin2dec.py
+++
@@ -1,9 +0,0 @@
-def bin_to_dec(digit):
-    length = len(digit)
-    number = 0
-    for i in range(0, length):
-        number = number + int(digit[i])*(2**(length-i-1)) #an*2n-1+an-1*2n-2+...+a0*20
-    return number
-
-a = input("Двоичное число:")
-print(f"Двоичное число {a}, равно десятичному числу {bin_to_dec(a)}")

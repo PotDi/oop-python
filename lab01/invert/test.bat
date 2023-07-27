1a51aaec1f628d2851cac2e5d946bc71af975db1
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-07-27 17:33:50 +0300

    Подправил тесты

--- lab01/invert/test.bat
+++ lab01/invert/test.bat
@@ -1,15 +1,12 @@
 @echo off
 
-set PROG = py invert.py
-set OUT = "invert_matrix_out.txt"
-
-echo Test matrix inverted
-%PROG% matrix.txt > %OUT% || goto test_failed
-fc %OUT% test_data\inverted_matrix.txt >nul || goto test_failed
-=======
 set PROG=py invert.py
 set INPUT_FILE="matrix.txt"
 set OUTPUT_FILE="test_out.txt"
 
 echo Test without command line arguments
 %PROG% > %OUTPUT_FILE% 2>nul && goto test failed
+
+echo Test matrix inverted
+%PROG% %INPUT_FILE% > %OUTPUT_FILE% || goto test_failed
+fc %OUTPUT_FILE% test_data\inverted_matrix.txt >nul || goto test_failed

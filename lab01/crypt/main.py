33a1b95c7b5ca1d57bafc66e6165338c4e4f4357
Dmitrii <dimasik007@rambler.ru>
2023-09-19 23:15:24 +0300

    Рефакторинг. Вынесение в отдельные функции

--- lab01/crypt/main.py
+++ lab01/crypt/main.py
@@ -1,6 +1,8 @@
 from crypt import FileCryptor
+import argparse
 
 def main():
+    args = parse_args()
     input_file = 'input.bin'  # Имя входного файла
     encrypted_file = 'encrypted.bin'  # Имя зашифрованного файла
     decrypted_file = 'decrypted.bin'  # Имя дешифрованного файла
@@ -16,6 +18,17 @@ def main():
     # Дешифрование файла
     file_encryptor_decryptor.decrypt_file(encrypted_file, decrypted_file)
     print('Файл успешно дешифрован.')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="Source file name")
+    parser.add_argument("encrypted_file", type=str, help="Encrypted file "
+                                                         "name")
+    parser.add_argument("decrypted_file", type=str, help="Decrypted file "
+                                                       "name")
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     main()

93b4980301eddd32cedf70fc98f7fa67843493bf
Dmitrii <dimasik007@rambler.ru>
2023-09-23 13:57:20 +0300

    Сделал ключ приватным и добавил комментарии

--- lab01/crypt/crypt.py
+++ lab01/crypt/crypt.py
@@ -1,13 +1,15 @@
 class FileCryptor:
     def __init__(self, key):
-        self.key = key
+        self.__key = key
 
-    def encrypt_file(self, input_file: str, output_file: str):
+    def encrypt_file(self, input_file: str, output_file: str): #доработать
+        # перемешивание битов
+        # убрать дублирование кода и вынести открытие и запись файла
         with open(input_file, 'rb') as f_in:
             with open(output_file, 'wb') as f_out:
                 byte = f_in.read(1)
                 while byte:
-                    encrypted_byte = bytes([byte[0] ^ self.key])
+                    encrypted_byte = bytes([byte[0] ^ self.__key])
                     f_out.write(encrypted_byte)
                     byte = f_in.read(1)
 
@@ -16,7 +18,7 @@ class FileCryptor:
             with open(output_file, 'wb') as f_out:
                 byte = f_in.read(1)
                 while byte:
-                    decrypted_byte = bytes([byte[0] ^ self.key])
+                    decrypted_byte = bytes([byte[0] ^ self.__key])
                     f_out.write(decrypted_byte)
                     byte = f_in.read(1)
 

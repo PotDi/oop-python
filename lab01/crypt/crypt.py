dd098642c1dd1e42e345db4705042b26cdb39ce0
Dmitrii <dimasik007@rambler.ru>
2023-09-29 23:32:26 +0300

    Рефакторинг

--- lab01/crypt/crypt.py
+++ lab01/crypt/crypt.py
@@ -8,8 +8,7 @@ class FileCryptor:
     def encrypt_file(self, input_file: str, output_file: str): #доработать
         # перемешивание битов
         # убрать дублирование кода и вынести открытие и запись файла
-        with open(input_file, 'rb') as f_in:
-            with open(output_file, 'wb') as f_out:
+        with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
                 byte = f_in.read(1)
                 while byte:
                     encrypted_byte = self.transform_bytes(byte)
@@ -17,8 +16,7 @@ class FileCryptor:
                     byte = f_in.read(1)
 
     def decrypt_file(self, input_file: str, output_file: str):
-        with open(input_file, 'rb') as f_in:
-            with open(output_file, 'wb') as f_out:
+        with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
                 byte = f_in.read(1)
                 while byte:
                     decrypted_byte = self.transform_bytes(byte)

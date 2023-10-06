bccb11909cf8172df3503f984a8a4026c2e0ef73
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-10-06 10:47:03 +0300

    Дорабатывал crypt.py

--- lab01/crypt/crypt.py
+++ lab01/crypt/crypt.py
@@ -1,37 +1,34 @@
+def write_file(file, data):
+    file.write(data)
+
+
+def open_file(input_file, mode):
+    file = open(input_file, mode)
+    return file
+
+
 class FileCryptor:
     def __init__(self, key):
         self.__key = key
 
-    def open_file(self, input_file, mode):
-        file = open(input_file, mode)
-        return file
+    def transform_bytes(self, byte):
+        return bytes([byte[0] ^ self.__key])
 
     def read_file(self, file):
         byte = file.read(1)
         while byte:
+            self.transform_bytes(byte)
             byte = file.read(1)
+        return byte
 
-    def write_file(self,):
-
-    def transform_bytes(self, byte):
-        return bytes([byte[0] ^ self.__key])
-
-    def encrypt_file(self, input_file: str, output_file: str): #доработать
-        # перемешивание битов
-        # убрать дублирование кода и вынести открытие и запись файла
-        with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
-                byte = f_in.read(1)
-                while byte:
-                    encrypted_byte = self.transform_bytes(byte)
-                    f_out.write(encrypted_byte)
-                    byte = f_in.read(1)
+    def encrypt_file(self, input_file: str, output_file: str):
+        with open_file(input_file, 'rb') as f_in, open_file(output_file, 'wb') \
+                as f_out:
+            encrypted_byte = self.read_file(f_in)
+            write_file(f_out, encrypted_byte)
 
     def decrypt_file(self, input_file: str, output_file: str):
-        with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
-                byte = f_in.read(1)
-                while byte:
-                    decrypted_byte = self.transform_bytes(byte)
-                    f_out.write(decrypted_byte)
-                    byte = f_in.read(1)
-
-
+        with open_file(input_file, 'rb') as f_in, open_file(output_file, 'wb') \
+                as f_out:
+            decrypted_byte = self.read_file(f_in)
+            write_file(f_out, decrypted_byte)

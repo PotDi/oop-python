cd8583d316e04e3c06cd06c2141e37fd82abf4b9
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-10-09 08:47:11 +0300

    Доработал тесты

--- lab01/crypt/test.bat
+++ lab01/crypt/test.bat
@@ -4,10 +4,17 @@ set PROG=py main.py
 set INPUT_FILE="input.bin"
 set OUTPUT_FILE="encrypt.bin"
 set OUT="decrypt bin"
+set KEY="32"
 
 echo Test encrypt file
-%PROG% %INPUT_FILE% %OUTPUT_FILE% %OUT% 2>nul || goto test_failed
-fc %OUTPUT_FILE% "test_data\test_source_file.bin" <nul || goto test_failed
+%PROG% %INPUT_FILE% %OUTPUT_FILE% %OUT% %KEY% || goto test_failed
+fc %OUTPUT_FILE% "test_data\test_source_file.bin" <nul && goto test_failed
+
+echo Test decrypt file
+fc %OUT% "test_data\test_source_file.bin" <nul || goto test_failed
+
+echo Test without command line arguments
+%PROG% > %OUTPUT_FILE% 2>nul && goto test failed
 
 echo All tests passed
 exit /B 0

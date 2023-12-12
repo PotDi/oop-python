711b5b288fe5bbae7c03d5cb01328f64ddd78890
Dmitrii <dimasik007@rambler.ru>
2023-12-12 09:07:20 +0300

    Написал три фукнции - чтения из файла, перевода слов и запись в словарик

--- lab02/Dictionary/dictionary.py
+++ lab02/Dictionary/dictionary.py
@@ -1,11 +1,23 @@
-def read_dict(file):
-    pass
+def read_dict(path):
+    dictionary = {}
+    with open(path, 'r') as file:
+        for line in file:
+            key, value = map(str.strip, line.split(':'))
+            dictionary[key] = value
+        return dictionary
 
 
-def translate_word(word, dict):
-    pass
+def translate_word(word, dictionary):
+    translation = dictionary.get(word.lower())
+    if translation:
+        return translation
+    else:
+        return "Перевод не найден"
 
 
-def write_dict(file, word, translation):
-    pass
+def write_dict(path, word, translation):
+    with open(path, 'a') as file:
+        file.write(f'{word}:{translation}\n')
+    print(f"Слово '{word}' с переводом '{translation}' успешно добавлен в "
+          f"словарь")
 
